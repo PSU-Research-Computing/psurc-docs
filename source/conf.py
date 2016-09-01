ae4880f996b2f59d263f84694d60ffc3c2c101e0
willpatterson <wpatt2@pdx.edu>
2016-08-31 18:19:43 -0700

    Changed name to PSURC Docs

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'OITRC Docs'
+project = u'PSURC Docs'
 copyright = u'2016, PSU:OIT:Research Computing'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -178,7 +178,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'OITRCDocsdoc'
+htmlhelp_basename = 'PSURCDocsdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -198,7 +198,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'OITRCDocs.tex', u'OIT Docs',
+  ('index', 'PSURCDocs.tex', u'PSURC Docs',
    u'PSU:OIT:Research Computing', 'manual'),
 ]
 
@@ -228,7 +228,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'oitrcdocs', u'OITRC Docs',
+    ('index', 'PSURCDocs', u'PSURC Docs',
      [u'PSU:OIT:Research Computing'], 1)
 ]
 
@@ -242,8 +242,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'OITRCDocs', u'OITRC Docs',
-   u'PSU:OIT:Research Computing', 'OITRCDocs', 'Documentation for Portland States Academic Resarch Servers.',
+  ('index', 'PSURCDocs', u'PSURC Docs',
+   u'PSU:OIT:Research Computing', 'PSURCDocs', 'Documentation for Portland States Academic Resarch Servers.',
    'Miscellaneous'),
 ]
 
